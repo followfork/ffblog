59e9bc4d3456b645bad672d75b8cc2fe71caed7b
followfork <xuwei_james@163.com>
2016-06-02 00:29:18 +0800

    登录、登出 功能，及 flash消息提醒

--- routes/index.js
+++ routes/index.js
@@ -10,11 +10,21 @@ var crypto = require('crypto'),
 
 module.exports = function(app) {
 	  app.get('/', function (req, res) {
-	    res.render('index', { title: '主页' });
+	    res.render('index', { 
+	    	title: '主页',
+	    	user: req.session.user,
+	    	success: req.flash('success').toString(),
+	    	error: req.flash('error').toString()
+	     });
 	  });
 
 	  app.get('/reg', function (req, res) {
-	    res.render('reg', { title: '注册' });
+	    res.render('reg', { 
+	    	title: '注册',
+	    	user: req.session.user,
+	    	success: req.flash('success').toString(),
+	    	error: req.flash('error').toString()
+	     });
 	  });
 
 	  app.post('/reg', function (req, res) {
@@ -58,15 +68,42 @@ module.exports = function(app) {
 	  });
 
 	  app.get('/login', function (req, res) {
-	    res.render('login', { title: '登录' });
+	    res.render('login', {
+	      title: '登录',
+	      user: req.session.user,
+	      success: req.flash('success').toString(),
+          error: req.flash('error').toString()
+	 	});
 	  });
+
 	  app.post('/login', function (req, res) {
+	  	// 生成md5
+	  	var md5 = crypto.createHash('md5'),
+	  		password = md5.update(req.body.password).digest('hex');
+	  	// 检查用户是否存在
+	  	User.get(req.body.name, function (err, user) {
+	  		if(!user) {
+	  			req.flash('error', '用户不存在');
+	  			return res.redirect('/login');
+	  		}
+	  		if(user.password != password) {
+	  			req.flash('error','密码错误');
+	  			return res.redirect('/login');
+	  		}
+	  		req.session.user = user;
+	  		req.flash('success', '登录成功');
+	  		res.redirect('/');
+	  	});
 	  });
 	  app.get('/post', function (req, res) {
 	    res.render('post', { title: '发表' });
 	  });
 	  app.post('/post', function (req, res) {
 	  });
+
 	  app.get('/logout', function (req, res) {
+	  	req.session.user = null;
+	  	req.flash('success', '登出成功');
+	  	res.redirect('/');
 	  });
 }
