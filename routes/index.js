07c836666cc2dd491f60c51acb45f1eb2ffe6cb4
followfork <xuwei_james@163.com>
2016-06-05 18:27:07 +0800

    添加404页面

--- routes/index.js
+++ routes/index.js
@@ -369,6 +369,11 @@ module.exports = function(app) {
 		  });
 		});
 
+	  // 添加 404
+	  app.use(function (req, res) {
+		res.render("404");
+	  });
+
 	  function checkLogin(req, res, next) {
 	  	if(!req.session.user) {
 	  		req.flash('error', '未登录');
